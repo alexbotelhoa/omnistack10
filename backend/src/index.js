176d30234642a7436e0d4ac3ef099b0427951e5a
Alex Botelho <alexbotelho1@hotmail.com>
2020-05-25 10:58:29 -0300

    Backend 30%

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+// const { setupWebsocket } = require('./websocket')
+
+const app = express();
+const server = http.Server(app);
+
+// setupWebsocket(server);
+
+mongoose.connect('mongodb+srv://admin:200679@cluster0-bbus6.mongodb.net/omnistack10?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
